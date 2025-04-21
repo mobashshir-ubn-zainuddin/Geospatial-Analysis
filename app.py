ed477bcf8ff495513d2ff5ac94e51f6177131e64
mobashshir-ubn-zainuddin <mobo@kgpian.iitkgp.ac.in>
2025-04-21 16:11:09 +0530

    Removed unnecessary files

--- app.py
+++ app.py
@@ -87,8 +87,7 @@ def get_progress():
 def mineral_mapping():
     return render_template('mineral_mapping.html')
 
-import os
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
+    print(f"Running on port {port}...")
     app.run(host="0.0.0.0", port=port)
